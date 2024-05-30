2ab5fcb8ca0ca819654c79c168dbf6308e09c29b
username <useremail>
2024-05-29 18:43:58 -0700

    Add project files.

---
+++ Country.py
@@ -0,0 +1,9 @@
+def display_menu():
+    print("Command Menu")
+    print("view --> View country name")
+    print("add --> Add a country")
+    print("delete --> Delete a country")
+    print("exit --> Exit the program")
+    print()
+    
+
